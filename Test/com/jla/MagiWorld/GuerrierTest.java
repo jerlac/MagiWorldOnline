2da2f205bb2e58883fe6fc79b634dd6b68d81bfc
JLA <jerome.lacambra@bnpparibas.com>
2019-05-15 14:02:47 +0200

    ajouts de tests Problème d'import de JUNIT

---
+++ Test/com/jla/MagiWorld/GuerrierTest.java
@@ -0,0 +1,14 @@
+package com.jla.MagiWorld;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GuerrierTest {
+
+    @org.junit.jupiter.api.Test
+    void attaqueBasique() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void attaqueSpeciale() {
+    }
+}
